1af4b416c671b3c65b3e064d035ebc575b7e4bce
Mubashir Nawaz <mubashirnawaz751@gmail.com>
2023-07-14 17:33:18 +0400

    first commit

---
+++ lib/Chapter3/chapter_3.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Chatpter3 extends StatefulWidget {
+  const Chatpter3({super.key});
+
+  @override
+  State<Chatpter3> createState() => _Chatpter3State();
+}
+
+class _Chatpter3State extends State<Chatpter3> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Chapter 3: Intro to Dart '),
+      ),
+    );
+  }
+}
