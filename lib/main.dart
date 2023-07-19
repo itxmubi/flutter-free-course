6867ea0f956b1034e8b36b3687620b8d489e64be
Mubashir Nawaz <mubashirnawaz751@gmail.com>
2023-07-19 12:13:26 +0400

    EOD chapter 12

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,11 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-import 'Chapter3/chapter_3.dart';
-
-const age = 20;
-
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
   runApp(const MyApp());
-}
-
-Stream<String> getName() {
-  return Stream.value("Mubashir");
-}
-
-void test() async {
-  await for (final value in getName()) {
-    print(value);
-  }
 }
 
 class MyApp extends StatelessWidget {
@@ -30,7 +20,78 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const Chatpter3(),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  late final TextEditingController emailController;
+  late final TextEditingController passwordController;
+
+  @override
+  void initState() {
+    emailController = TextEditingController();
+    passwordController = TextEditingController();
+
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    emailController.dispose();
+    passwordController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Register"),
+      ),
+      body: FutureBuilder(
+          future: Firebase.initializeApp(),
+          builder: (context, snapshot) {
+            switch (snapshot.connectionState) {
+              case ConnectionState.done:
+                return Column(children: [
+                  TextField(
+                    controller: emailController,
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: const InputDecoration(
+                        hintText: "Enter your Email here"),
+                  ),
+                  TextField(
+                    controller: passwordController,
+                    obscureText: true,
+                    autocorrect: false,
+                    decoration: const InputDecoration(
+                        hintText: "Enter your password here"),
+                  ),
+                  TextButton(
+                    onPressed: () async {
+                      final email = emailController.text;
+                      final password = passwordController.text;
+
+                      await FirebaseAuth.instance
+                          .createUserWithEmailAndPassword(
+                              email: email, password: password);
+                    },
+                    child: const Text("Register"),
+                  )
+                ]);
+              default:
+                return const Text("Loading....");
+            }
+          }),
     );
   }
 }
